939e8a2742b302a47eabd777879ee5a8c95a73d7
faten_saleh <fatensaleh996@gmail.com>
2019-06-03 14:26:59 +0300

    HOME PAGE GUI FOR MANAGERS

---
+++ G13_Assignment3/src/application/CompanyMhomePage.java
@@ -0,0 +1,109 @@
+/**
+ * Sample Skeleton for 'CompanyMhomePage.fxml' Controller Class
+ */
+
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CompanyMhomePage {
+
+    @FXML // fx:id="CMeditPath"
+    private Button CMeditPath; // Value injected by FXMLLoader
+
+    @FXML // fx:id="seeUserReports"
+    private Button seeUserReports; // Value injected by FXMLLoader
+
+    @FXML // fx:id="CMaddMapToCity"
+    private Button CMaddMapToCity; // Value injected by FXMLLoader
+
+    @FXML // fx:id="CMSeeMaps"
+    private Button CMSeeMaps; // Value injected by FXMLLoader
+
+    @FXML // fx:id="acceptPrice"
+    private Button acceptPrice; // Value injected by FXMLLoader
+
+    @FXML // fx:id="CMeditInterestingPlace"
+    private Button CMeditInterestingPlace; // Value injected by FXMLLoader
+
+    @FXML // fx:id="CMaddPath"
+    private Button CMaddPath; // Value injected by FXMLLoader
+
+    @FXML // fx:id="CMchangePrice"
+    private Button CMchangePrice; // Value injected by FXMLLoader
+
+    @FXML // fx:id="CMacceptVersion"
+    private Button CMacceptVersion; // Value injected by FXMLLoader
+
+    @FXML // fx:id="outCDMHome"
+    private Button outCDMHome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="CMcreateCity"
+    private Button CMcreateCity; // Value injected by FXMLLoader
+
+    @FXML // fx:id="CMaddInterestingPlace"
+    private Button CMaddInterestingPlace; // Value injected by FXMLLoader
+
+    @FXML
+    void LogOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CMaddMapToCity(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CMSeeMaps(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CMcreateCity(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CMeditInterestingPlace(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CMaddInterestingPlace(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CMeditPath(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CMaddPath(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CMacceptVersion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CMchangePrice(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seeUserReports(ActionEvent event) {
+
+    }
+
+    @FXML
+    void acceptPrice(ActionEvent event) {
+
+    }
+
+}
