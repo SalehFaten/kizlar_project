4d12c4730dc671b49f2be92b48bfb41533b95e25
ayabe <ayabe@192.168.34.193>
2019-06-03 14:03:18 +0300

    GUI

---
+++ G13_Assignment3/src/application/AddIneristingPlace.java
@@ -0,0 +1,78 @@
+/**
+ * Sample Skeleton for 'AddIneristingPath.fxml' Controller Class
+ */
+
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddIneristingPlace {
+
+    @FXML // fx:id="Addbtn"
+    private Button Addbtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="enterPlaceId"
+    private TextField enterPlaceId; // Value injected by FXMLLoader
+
+    @FXML // fx:id="enterRecomendedTime"
+    private TextField enterRecomendedTime; // Value injected by FXMLLoader
+
+    @FXML // fx:id="enterCityId"
+    private TextField enterCityId; // Value injected by FXMLLoader
+
+    @FXML // fx:id="enterDiscription"
+    private TextField enterDiscription; // Value injected by FXMLLoader
+
+    @FXML // fx:id="enterPlaceName"
+    private TextField enterPlaceName; // Value injected by FXMLLoader
+
+    @FXML // fx:id="enterPathToPlace"
+    private TextField enterPathToPlace; // Value injected by FXMLLoader
+
+    @FXML // fx:id="enterLocation"
+    private TextField enterLocation; // Value injected by FXMLLoader
+
+    @FXML
+    void Addbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterPlaceName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterCityId(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterPlaceId(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterDiscription(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterLocation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterRecomendedTime(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enterPathToPlace(ActionEvent event) {
+
+    }
+
+}
